9e100fe7d2d043f71b42d3d7a184311b39e1ef63
aaaether <bekaaliyev848007@gmail.com>
2025-06-13 01:02:09 +0500

    lesson4 hw

---
+++ PhpBasicsLesson4.php
@@ -0,0 +1,32 @@
+<?php
+
+
+//Переменные для студента
+
+$studentName = 'Beksultan';
+$studentAge = 18;
+$studentAverageScore = 4.8;
+
+//Переменные для машины
+
+$carBrand = 'Hyundai';
+$carYearRelease = '2023';
+$carMileageInKilometers = 60000;
+
+//Перменные для описания покупки в интернет-магазине
+
+$product = 'Phone';
+$productCount = 1;
+$productPricing = 500000;
+
+//Переменные для хранения личных данных
+
+$userName = 'Beksultan';
+$userEmail = 'bekaaliyev848007@gmail.com';
+$userBirthday = '2006-07-08';
+
+//Переменные для описания продукта
+
+$productName = 'Iphone';
+$productCategory = 'Electronic';
+$productCountry = 'Indonesia';
